77cf3dff5832020c51cab2b9b7080ab78a117bac
willymwai <willymwaid92@gmail.com>
2020-04-13 08:13:19 +0300

    patch -- dashboard

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir /usr/local/CyberCP
+cp -r /usr/local/bin/ /usr/local/CyberCP/bin/
+touch /etc/redhat-release
+
 python manage.py migrate
 export C_FORCE_ROOT="true"
 gunicorn CyberCP.wsgi -b 0.0.0.0:8000 -w 2 -t 240
